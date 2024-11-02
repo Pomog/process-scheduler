dd1041a62eef90b07b83f15869e9cf2f99d19f86
Yurii Panasuik <thoryur@gmail.com>
2024-11-02 17:53:47 +0200

    Initial commit

---
+++ src/main/java/com/pomog/schedulerV1/process_scheduler/ProcessSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.pomog.schedulerV1.process_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pomog/schedulerV1/process_scheduler/ProcessSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomog.schedulerV1.process_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
