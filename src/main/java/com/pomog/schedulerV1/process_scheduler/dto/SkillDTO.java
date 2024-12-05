e8f3b78db4b993ae1b9f0d9553b845101c4c9e95
Yurii Panasuik <thoryur@gmail.com>
2024-12-05 22:15:49 +0200

    (operator) -> operator.setSkills(null)

--- src/main/java/com/pomog/schedulerV1/process_scheduler/dto/SkillDTO.java
+++ src/main/java/com/pomog/schedulerV1/process_scheduler/dto/SkillDTO.java
@@ -7,7 +7,10 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Data
 @RequiredArgsConstructor
@@ -24,6 +27,8 @@ public class SkillDTO {
     @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
     private String stepName;
     
+    private List<OperatorDTO> operators = new ArrayList<>();
+    
     private int level;
     
     // Learning EVE
@@ -32,5 +37,14 @@ public class SkillDTO {
         this.processName = skillEntity.getProcessName();
         this.stepName = skillEntity.getStepName();
         this.level = skillEntity.getLevel();
+        this.operators = skillEntity.getOperatorEntities().stream()
+                .map(OperatorDTO::new)
+                .collect(Collectors.toList());
+        toDtoWithoutOperators();
     }
+    
+    public void toDtoWithoutOperators() {
+        this.operators.forEach((operator) -> operator.setSkills(null));
+    }
+    
 }
